abd17830a8d7d4de501776eb7cbb2489a5feab01
peace <astonreba@gmail.com>
2020-07-14 10:08:29 +0300

    Work on task

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
+from datetime import datetime
+
+app = Flask(__name__)
+mail = Mail(app)
+
+items = [
+    {
+        "name": "Apples",
+        "unit_price": 100,
+        "date_created": "12/07/2020"
+    },
+    {
+        "name": "Mangoes",
+        "unit_price": 100,
+        "date_created": datetime.utcnow()
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=['POST', 'GET'])
+def add_item():
+    if request.method == 'POST':
+        new_item = {
+            "name": request.form['name'],
+            "unit_price":request.form['price'],
+            "date_created": datetime.utcnow()
+        }
+        items.append(new_item)
+
+        # msg = Message('A new shop item has been added', sender='astonreba@gmail.com', recipients=['markkatamba@akorion.com'])
+        # msg.body = new_item
+        # mail.send(msg)
+
+        return redirect('/')
+    else:
+        return render_template('add.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
